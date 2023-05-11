33741069afa26803a9e8e52477576411fac36f1a
katerina-shche <asti.katerina@gmail.com>
2023-05-11 12:47:07 +0200

    drums mode added

--- src/App.js
+++ src/App.js
@@ -1,78 +1,82 @@
 import DrumPad from "./components/DrumPad.js"
 import { v4 as uuidv4 } from 'uuid'
 import { useEffect, useState } from "react";
-
-
-
 //styles
 import './App.css';
 //sounds
-import pianoAmajor from './assets/piano-a_A_major.wav' 
-import pianoBmajor from './assets/piano-b_B_major.wav'
-import pianoCmajor from './assets/piano-c_C_major.wav'
-import pianoDmajor from './assets/piano-d_D_major.wav'
-import pianoEmajor from './assets/piano-e_E_major.wav'
-import pianoFmajor from './assets/piano-f_F_major.wav'
-import pianoFsharpmajor from './assets/piano-f_Fsharp_major.wav'
-import pianoGmajor from './assets/piano-g_G_major.wav'
-import pianoGsharpmajor from './assets/piano-g_Gsharp_major.wav'
-
-
+import pianoAmajor from './assets/piano/piano-a_A_major.wav' 
+import pianoBmajor from './assets/piano/piano-b_B_major.wav'
+import pianoCmajor from './assets/piano/piano-c_C_major.wav'
+import pianoDmajor from './assets/piano/piano-d_D_major.wav'
+import pianoEmajor from './assets/piano/piano-e_E_major.wav'
+import pianoFmajor from './assets/piano/piano-f_F_major.wav'
+import pianoFsharpmajor from './assets/piano/piano-f_Fsharp_major.wav'
+import pianoGmajor from './assets/piano/piano-g_G_major.wav'
+import pianoGsharpmajor from './assets/piano/piano-g_Gsharp_major.wav'
+import clap from './assets/drums/Clap.mp3'
+import closedHH from './assets/drums/Closed-HH.mp3'
+import heater1 from './assets/drums/Heater-1.mp3'
+import heater2 from './assets/drums/Heater-2.mp3'
+import heater3 from './assets/drums/Heater-3.mp3'
+import heater4 from './assets/drums/Heater-4.mp3'
+import kickNHat from './assets/drums/Kick-n-Hat.mp3'
+import kick from './assets/drums/Kick.mp3'
+import openHH from './assets/drums/Open-HH.mp3'
 
 function App() {
  
   const piano = [
     {
       kbdletter: 'Q',
-      sound: 'piano-A-major',
+      sound: 'Piano-A-Major',
       audiosrc: pianoAmajor,
       'data-key': '81'
     }, 
     {
       kbdletter: 'W',
-      sound: 'piano-B-major',
+      sound: 'Piano-B-Major',
       audiosrc: pianoBmajor,
       'data-key': '87'
     },
     {
       kbdletter: 'E',
-      sound: 'piano-C-major',
+      sound: 'Piano-C-Major',
       audiosrc: pianoCmajor,
       'data-key': '69'
     },
     {
       kbdletter: 'A',
-      sound: 'piano-D-major',
+      sound: 'Piano-D-Major',
       audiosrc: pianoDmajor,
       'data-key': '65'
     },
     {
       kbdletter: 'S',
-      sound: 'piano-E-major',
+      sound: 'Piano-E-Major',
       audiosrc: pianoEmajor,
       'data-key': '83'
     },
     {
       kbdletter: 'D',
-      sound: 'piano-F-major',
+      sound: 'Piano-F-Major',
       audiosrc: pianoFmajor,
       'data-key': '68'
     },
     {
       kbdletter: 'Z',
-      sound: 'piano-F-sharp',
+      sound: 'Piano-F-Sharp',
       audiosrc: pianoFsharpmajor,
       'data-key': '90'
     },
     {
       kbdletter: 'X',
-      sound: 'piano-G-major',
+      sound: 'Piano-G-Major',
       audiosrc: pianoGmajor,
       'data-key': '88'
     }, 
     {
       kbdletter: 'C',
-      sound: 'piano-G-sharp',
+      sound: 'Piano-G-Sharp',
       audiosrc: pianoGsharpmajor,
       'data-key': '67'
     }
@@ -80,56 +84,56 @@ function App() {
   const drums = [
     {
       kbdletter: 'Q',
-      sound: 'piano-A-major',
-      audiosrc: pianoAmajor,
+      sound: 'Clap',
+      audiosrc: clap,
       'data-key': '81'
     }, 
     {
       kbdletter: 'W',
-      sound: 'piano-B-major',
-      audiosrc: pianoBmajor,
+      sound: 'Closed-HH',
+      audiosrc: closedHH,
       'data-key': '87'
     },
     {
       kbdletter: 'E',
-      sound: 'piano-C-major',
-      audiosrc: pianoCmajor,
+      sound: 'Heater-1',
+      audiosrc: heater1,
       'data-key': '69'
     },
     {
       kbdletter: 'A',
-      sound: 'piano-D-major',
-      audiosrc: pianoDmajor,
+      sound: 'Heater-2',
+      audiosrc: heater2,
       'data-key': '65'
     },
     {
       kbdletter: 'S',
-      sound: 'piano-E-major',
-      audiosrc: pianoEmajor,
+      sound: 'Heater-3',
+      audiosrc: heater3,
       'data-key': '83'
     },
     {
       kbdletter: 'D',
-      sound: 'piano-F-major',
-      audiosrc: pianoFmajor,
+      sound: 'Heater-4',
+      audiosrc: heater4,
       'data-key': '68'
     },
     {
       kbdletter: 'Z',
-      sound: 'piano-F-sharp',
-      audiosrc: pianoFsharpmajor,
+      sound: 'Kick-n-Hat',
+      audiosrc: kickNHat,
       'data-key': '90'
     },
     {
       kbdletter: 'X',
-      sound: 'piano-G-major',
-      audiosrc: pianoGmajor,
+      sound: 'Kick',
+      audiosrc: kick,
       'data-key': '88'
     }, 
     {
       kbdletter: 'C',
-      sound: 'piano-G-sharp',
-      audiosrc: pianoGsharpmajor,
+      sound: 'Open-HH',
+      audiosrc: openHH,
       'data-key': '67'
     }
   ]
